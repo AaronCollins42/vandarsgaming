9eb0941a47fc60e81e535dc092da5467894e18af
nimrod3k <col08020@byui.edu>
2024-12-30 12:37:08 -0500

    Finished the file load methods for this app

--- hs/doorfinder/doorfound.php
+++ hs/doorfinder/doorfound.php
@@ -1,18 +1,7 @@
 <?php
 include '../save_file_upload.php';
-// $target_dir = "hs_save/";
-// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-// $uploadOk = 1;
-// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// // Check if image file is a actual image or fake image
-// if(isset($_POST["submit"])) {
-//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//   if($check !== false) {
-//     echo "File is an image - " . $check["mime"] . ".";
-//     $uploadOk = 1;
-//   } else {
-//     echo "File is not an image.";
-//     $uploadOk = 0;
-//   }
-// }
+
+$data = read_save_file();
+// $door_lines = file_get_contents("doors.txt"); 
+$missing_doors = array();
 ?>
--- hs/save_file_upload.php
+++ hs/save_file_upload.php
@@ -1,23 +1,24 @@
 <?php
 include 'hsfileutils.php';
 
-// print $_FILES["saveFile"]["tmp_name"];
-try{
-    //checks for errors
-    if ($_FILES['saveFile']['error'] == UPLOAD_ERR_OK               
-    && is_uploaded_file($_FILES['saveFile']["tmp_name"])) { //checks that file is uploaded
-        $save_contents = file_get_contents($_FILES['saveFile']["tmp_name"]); 
-        $total_steps = substr($save_contents, 0, 10);
+function read_save_file() {
+    try{
+        //checks for errors
+        if ($_FILES['saveFile']['error'] == UPLOAD_ERR_OK               
+        && is_uploaded_file($_FILES['saveFile']["tmp_name"])) { //checks that file is uploaded
+            $save_contents = file_get_contents($_FILES['saveFile']["tmp_name"]); 
+            $total_steps = substr($save_contents, 0, 10);
 
-        $save_data = unscramble($save_contents, (int)$total_steps);
-        $data = json_decode($save_data, true);
-        print $save_data;
+            $save_data = unscramble($save_contents, (int)$total_steps);
+            $data = json_decode($save_data, true);
+            return $data;
 
+
+        }
+    } catch(Exception $e){
+        print $e->getMessage();
     }
-} catch(Exception $e){
-    echo $e->getMessage();
 }
-
 
 
 ?>
