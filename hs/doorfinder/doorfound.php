1f9031b1468a5fb1b3c623238861b122fc7e0790
nimrod3k <col08020@byui.edu>
2024-12-30 15:39:47 -0500

    Functionally Complete needs some polish though

--- hs/doorfinder/doorfound.php
+++ hs/doorfinder/doorfound.php
@@ -1,7 +1,50 @@
 <?php
 include '../save_file_upload.php';
 
+
+function rand_lookup($missing_doors)
+{
+    include 'hinttext.php';
+    if ($missing_doors != null && sizeof($missing_doors) > 0)
+    {
+            $rand_num = rand(0,sizeof($missing_doors)-1);
+
+            $rand_door = $missing_doors[$rand_num];
+            $split_rand_door = explode(":", $rand_door);
+            $door_map = explode(".",$split_rand_door[0])[0];
+            $doorColor = "Gold";
+
+            if ($split_rand_door[1] == "s")
+                $doorColor = "Silver";
+
+            $doorText = $HINT_TEXT[$door_map][0];
+
+            print "<br/>";
+            print "One ".$doorColor." Door is ".$doorText;
+    }
+}
+
+
 $data = read_save_file();
 // $door_lines = file_get_contents("doors.txt"); 
 $missing_doors = array();
+
+foreach(file("doors.txt") as $line) {
+    // print $line."<br/>";
+    $split_line = explode(':',$line);
+    //print_r( $split_line);//."<br/>"."<br/>";
+    if (!array_key_exists($split_line[0],$data["flags"])) 
+    {
+        $missing_doors[] = $line;
+    }
+}
+if (sizeof($missing_doors) > 0)
+    print "You have ". sizeof($missing_doors) ." doors left to find";
+else
+    print "You seem to have opened all Gold and Silver Doors";
+
+rand_lookup($missing_doors);
+
+
+
 ?>
---
+++ hs/doorfinder/hinttext.php
@@ -0,0 +1,42 @@
+<?php
+include "../maps.php";
+
+
+$HINT_TEXT = [
+    $Maps["DUST_SHELF"] => ["in a dusty landscape"],
+    $Maps["THRONE_ROOM"] => ["Throne Room"],
+    $Maps["THRONE_ROOM_Q"] => ["Throne Room"],
+    $Maps["CASTLE_RUINS"] => ["Castle Ruins"],
+    $Maps["NORTH_MUNDEMAN"] => ["inside a cave of lava"],
+    $Maps["SOUTH_MUNDEMAN"] => ["inside a watery cave"],
+    $Maps["VERDANT_COAST"] => ["in a land of cherry blossoms"],
+    $Maps["OTHERWORLD"] => ["Otherworld"],
+    $Maps["CASTLE_GROUNDS"] => ["on the Castle Grounds"],
+    $Maps["SANCTUARY"] => ["Sanctuary"],
+    $Maps["THE_TUNNELS"] => ["inside a dark tunnel"],
+    $Maps["GLITCH"] => ["Glitch"],
+    $Maps["LUDDERSHORE"] => ["in Luddershore"],
+    $Maps["THE_TUNDRA"] => ["somewhere on a vast tundra"],
+    $Maps["FROZEN_SHORE"] => ["someplace cold"],
+    $Maps["HALLOW_GROUND"] => ["somewhat haunted"],
+    $Maps["SOUTHERN_SWAMP"] => ["inside a swamp"],
+    $Maps["DRAGONS_LAIR"] => ["hanging with a smurf dragon"],
+    $Maps["CORRUPTED_CASTLE"] => ["Corrupted Castle"],
+    $Maps["CASTLE_MONILLUD"] => ["waiting for you in the castle"],
+    $Maps["THRONE_ROOM_A"] => ["Throne Room"],
+    $Maps["THE_UNDERWORLD"] => ["in the basement of the world"],
+    $Maps["OTHERWORLD_p"] => ["Otherworld"],
+    $Maps["MOLTEN_CAVERN"] => ["hot as lava (actually I think it's called magma)"],
+    $Maps["THE_DUNGEONS"] => ["chillin' with Frank"],
+    $Maps["ITEM_SHOP"] => ["wants to go shopping"],
+    $Maps["CONVERGENCE"] => ["Convergence"],
+    $Maps["TRIAL_OF_REALITY"] => ["Trial of Reality"],
+    $Maps["THE_FALLEN_WORLD"] => ["Fallen World"],
+    $Maps["THE_ROAD_TO_HELL"] => ["Road to Hell"],
+    $Maps["HAUNTED_MANSE"] => ["somewhat haunted"],
+    $Maps["THE_MOONWELL"] => ["Moonwell"],
+    $Maps["SMUGGLERS_ROAD"] => ["Smuggler's Road"],
+    $Maps["SMUGGLERS_RUIN"] => ["Smuggler's Road"]
+];
+
+?>
